2a024f7ea4720cf38581187451a9c2cd613dd6ee
DoubleJack <shuangjiezeng@gmail.com>
2024-04-23 22:34:48 +0800

    feat: auto migrate

---
+++ user_srv/model/main/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"srvs/user_srv/model"
+)
+
+func main() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      false,       // Don't include params in the SQL log
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	_ = db.AutoMigrate(&model.User{})
+}
