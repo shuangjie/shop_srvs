ad39f0e475080178afdc1dc30abeb76a54921813
DoubleJack <shuangjiezeng@gmail.com>
2024-04-23 22:01:58 +0800

    feat: user struct

---
+++ user_srv/model/user.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID        int32     `gorm:"primarykey"`
+	CreatedAt time.Time `gorm:"column:add_time"`
+	UpdatedAt time.Time `gorm:"column:update_time"`
+	DeletedAt gorm.DeletedAt
+	IsDeleted bool
+}
+
+type User struct {
+	BaseModel
+	Mobile   string     `gorm:"index:idx_mobile;unique;type:varchar(11);not null comment '手机号'"`
+	Password string     `gorm:"type:varchar(100);not null comment '密码'"`
+	NickName string     `gorm:"type:varchar(20) comment '昵称'"`
+	Birthday *time.Time `gorm:"type:datetime comment '生日'"`
+	Gender   string     `gorm:"column:gender;default:male;type:varchar(6) comment 'female表示女, male表示男'"`
+	Role     int        `gorm:"column:role;default:1;type:int comment '1表示普通用户, 2表示管理员'"`
+}
