c0c3bb4d19b947b70f3b9993804259bb57870b32
DoubleJack <shuangjiezeng@gmail.com>
2024-04-24 20:41:31 +0800

    chore: register UserServer

--- user_srv/main.go
+++ user_srv/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"srvs/user_srv/handler"
+	"srvs/user_srv/proto"
+)
+
 func main() {
+	IP := flag.String("ip", "0.0.0.0", "ip address")
+	Port := flag.Int("port", 50051, "port")
+
+	flag.Parse()
+	fmt.Printf("ip: %s, port: %d\n", *IP, *Port)
+
+	server := grpc.NewServer()
+	proto.RegisterUserServer(server, &handler.UserServer{})
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *IP, *Port))
+	if err != nil {
+		panic("failed to listen: " + err.Error())
+	}
+	err = server.Serve(lis)
+	if err != nil {
+		panic("failed to serve: " + err.Error())
+	}
 
 }
