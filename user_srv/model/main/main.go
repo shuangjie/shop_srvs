27813a9594c38629e8e7f6501285369f298afd38
DoubleJack <shuangjiezeng@gmail.com>
2024-04-24 20:51:33 +0800

    chore: 批量创建用户

--- user_srv/model/main/main.go
+++ user_srv/model/main/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"crypto/sha512"
 	"fmt"
-	"strings"
+	"log"
+	"os"
+	"time"
 
 	"github.com/anaskhan96/go-password-encoder"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+
+	"srvs/user_srv/model"
 )
 
 func main() {
-	/*dsn := "root:123456@tcp(127.0.0.1:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
 
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
@@ -22,6 +30,7 @@ func main() {
 		},
 	)
 
+	// start 批量创建用户
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
@@ -32,16 +41,31 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	_ = db.AutoMigrate(&model.User{})*/
+	options := &password.Options{16, 100, 32, sha512.New}
+	salt, encodedPwd := password.Encode("admin123", options)
+	newPassword := fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd)
+	fmt.Println(newPassword)
+
+	for i := 0; i < 10; i++ {
+		user := model.User{
+			NickName: fmt.Sprintf("nick_%d", i),
+			Mobile:   fmt.Sprintf("1888888888%d", i),
+			Password: newPassword,
+		}
+		db.Save(&user)
+	}
+	// end 批量创建用户
+
+	//_ = db.AutoMigrate(&model.User{})
 
 	// Using custom options
-	options := &password.Options{16, 100, 32, sha512.New}
-	salt, encodedPwd := password.Encode("generic password", options)
-	newPassword := fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd)
-	fmt.Println(len(newPassword))
-	fmt.Println(newPassword)
-	passwordInfo := strings.Split(newPassword, "$")
-	fmt.Println(passwordInfo)
-	check := password.Verify("generic password", passwordInfo[2], passwordInfo[3], options)
-	fmt.Println(check) // true
+	//options := &password.Options{16, 100, 32, sha512.New}
+	//salt, encodedPwd := password.Encode("generic password", options)
+	//newPassword := fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd)
+	//fmt.Println(len(newPassword))
+	//fmt.Println(newPassword)
+	//passwordInfo := strings.Split(newPassword, "$")
+	//fmt.Println(passwordInfo)
+	//check := password.Verify("generic password", passwordInfo[2], passwordInfo[3], options)
+	//fmt.Println(check) // true
 }
