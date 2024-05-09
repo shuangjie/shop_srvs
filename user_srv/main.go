32b2e17c32d9609ee0d45a42fd155000f2f496b6
DoubleJack <shuangjiezeng@gmail.com>
2024-05-09 19:17:50 +0800

    chore: consul

--- user_srv/main.go
+++ user_srv/main.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/hashicorp/consul/api"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 
+	"srvs/user_srv/global"
 	"srvs/user_srv/handler"
 	"srvs/user_srv/initialize"
 	"srvs/user_srv/proto"
@@ -22,8 +26,10 @@ func main() {
 	initialize.InitConfig()
 	initialize.InitDB()
 
+	zap.S().Info(global.ServerConfig)
+
 	flag.Parse()
-	zap.S().Info("ip: %s, port: %d\n", *IP, *Port)
+	zap.S().Info(*IP, *Port)
 
 	server := grpc.NewServer()
 	proto.RegisterUserServer(server, &handler.UserServer{})
@@ -32,6 +38,40 @@ func main() {
 	if err != nil {
 		panic("failed to listen: " + err.Error())
 	}
+
+	//注册健康检查服务
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	//服务注册
+	cfg := api.DefaultConfig()
+	cfg.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
+
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	check := &api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("192.168.2.49:50051"),
+		Interval:                       "5s",
+		Timeout:                        "3s",
+		DeregisterCriticalServiceAfter: "10s",
+	}
+
+	registration := new(api.AgentServiceRegistration)
+	registration.ID = global.ServerConfig.Name
+	registration.Name = global.ServerConfig.Name
+	registration.Port = *Port
+	registration.Tags = []string{"zeng", "user", "grpc"}
+	registration.Address = "192.168.2.49"
+	registration.Check = check
+
+	err = client.Agent().ServiceRegister(registration)
+
+	if err != nil {
+		panic(err)
+	}
+
 	err = server.Serve(lis)
 	if err != nil {
 		panic("failed to serve: " + err.Error())
