89c117ca34652566f451ccd4162e201a9454fa7b
DoubleJack <shuangjiezeng@gmail.com>
2024-05-11 18:00:13 +0800

    chore: goods struct

--- goods_srv/model/base.go
+++ goods_srv/model/base.go
@@ -1,10 +1,22 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"time"
 
 	"gorm.io/gorm"
 )
+
+type GormList []string
+
+func (g GormList) Value() (driver.Value, error) {
+	return json.Marshal(g)
+}
+
+func (g GormList) Scan(value interface{}) error {
+	return json.Unmarshal(value.([]byte), &g)
+}
 
 type BaseModel struct {
 	ID        int32     `gorm:"primarykey;type:int"`
--- goods_srv/model/goods.go
+++ goods_srv/model/goods.go
@@ -2,17 +2,17 @@ package model
 
 type Category struct {
 	BaseModel
-	Name             string `gorm:"type:varchar(50);not null"`
+	Name             string `gorm:"type:varchar(50);not null;comment:'分类名称''"`
 	ParentCategoryID int32
 	ParentCategory   *Category
-	Level            int32 `gorm:"type:int;not null;default:1"`
-	IsTab            bool  `gorm:"type:tinyint;not null;default:0"`
+	Level            int32 `gorm:"type:int;not null;default:1;comment:'分类级别''"`
+	IsTab            bool  `gorm:"type:tinyint;not null;default:0;comment:'是否在tab栏显示'"`
 }
 
 type Brand struct {
 	BaseModel
-	Name string `gorm:"type:varchar(20);not null"`
-	Logo string `gorm:"type:varchar(200);not null;default:''"`
+	Name string `gorm:"type:varchar(20);not null;comment:'品牌名称'"`
+	Logo string `gorm:"type:varchar(200);not null;default:'';comment:'品牌logo'"`
 }
 
 // GoodsCategoryBrand 可以用 gorm 的 many2many 多对多关联来自动生成
@@ -32,7 +32,33 @@ func (GoodsCategoryBrand) TableName() string {
 // Banner Image 的长度为 200，实际上可能会有问题，可以根据实际情况调整
 type Banner struct {
 	BaseModel
-	Image string `gorm:"type:varchar(200);not null"`
-	Url   string `gorm:"type:varchar(200);not null"`
-	Index int32  `gorm:"type:int;not null;default:1"`
+	Image string `gorm:"type:varchar(200);not null;comment:'图片地址'"`
+	Url   string `gorm:"type:varchar(200);not null;comment:'跳转链接'"`
+	Index int32  `gorm:"type:int;not null;default:1;comment:'排序'"`
+}
+
+type Goods struct {
+	BaseModel
+
+	CategoryID int32 `gorm:"type:int;not null"`
+	Category   *Category
+	BrandID    int32 `gorm:"type:int;not null"`
+	Brand      *Brand
+
+	OnSale   bool `gorm:"type:tinyint;not null;default:0;comment:'是否上架'"`
+	ShipFree bool `gorm:"type:tinyint;not null;default:0;comment:'是否包邮'"`
+	IsNew    bool `gorm:"type:tinyint;not null;default:0;comment:'是否新品'"`
+	IsHot    bool `gorm:"type:tinyint;not null;default:0;comment:'是否热销'"`
+
+	Name            string   `gorm:"type:varchar(50);not null;comment:'商品名称'"`
+	GoodsSn         string   `gorm:"type:varchar(50);not null;comment:'商品编号'"`
+	Clicks          int32    `gorm:"type:int;not null;default:0;comment:'点击数'"`
+	SoldNum         int32    `gorm:"type:int;not null;default:0;comment:'销售数量'"`
+	FavNum          int32    `gorm:"type:int;not null;default:0;comment:'收藏数量'"`
+	MarketPrice     float32  `gorm:"type:decimal(10,2);not null;default:0.01;comment:'市场价'"`
+	ShopPrice       float32  `gorm:"type:decimal(10,2);not null;default:0.01;comment:'商城价'"`
+	GoodsBrief      string   `gorm:"type:varchar(200);not null;default:'';comment:'商品简介'"`
+	Images          GormList `gorm:"type:varchar(1000);not null;default:'';comment:'商品图片'"`
+	DescImages      GormList `gorm:"type:varchar(1000);not null;default:'';comment:'商品描述图片'"`
+	GoodsFrontImage string   `gorm:"type:varchar(200);not null;default:'';comment:'商品主图'"`
 }
