33f220841ea322173beb9219c62c1514d79accd1
DoubleJack <shuangjiezeng@gmail.com>
2024-06-15 00:49:03 +0800

    chore: tests 测试用例

--- order_srv/handler/order_test.go
+++ order_srv/handler/order_test.go
@@ -22,6 +22,7 @@ func Init() {
 	orderClient = proto.NewOrderClient(conn)
 }
 
+// TestCreateCartItem 测试添加购物车
 func TestCreateCartItem(t *testing.T) {
 	Init()
 	defer conn.Close()
@@ -34,7 +35,7 @@ func TestCreateCartItem(t *testing.T) {
 	}{
 		{userId: 1, goodsId: 421, nums: 1, wantErr: false},
 		{userId: 2, goodsId: 422, nums: 2, wantErr: false},
-		//{userId: 3, goodsId: 1, nums: 3, wantErr: true}, // 示例：此处预期会出错
+		{userId: 3, goodsId: 1, nums: 3, wantErr: true}, // 示例：此处预期会出错
 	}
 
 	for _, tt := range tests {
@@ -49,6 +50,37 @@ func TestCreateCartItem(t *testing.T) {
 			}
 			if err == nil {
 				fmt.Println("CreateCartItem 成功，ID:", rsp.Id)
+			}
+		})
+	}
+}
+
+// TestCartItemList 测试获取购物车列表
+func TestCartItemList(t *testing.T) {
+	Init()
+	defer conn.Close()
+
+	tests := []struct {
+		userId  int32
+		wantErr bool
+	}{
+		{userId: 1, wantErr: false},
+		{userId: 2, wantErr: false},
+		{userId: 3, wantErr: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("用户ID=%d", tt.userId), func(t *testing.T) {
+			rsp, err := orderClient.CartItemList(context.Background(), &proto.UserInfo{
+				Id: tt.userId,
+			})
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("CartItemList 错误 = %v, 预期错误 %v", err, tt.wantErr)
+			}
+			if err == nil {
+				for _, item := range rsp.Data {
+					fmt.Println(item.Id, item.GoodsId, item.Nums)
+				}
 			}
 		})
 	}
--- order_srv/tests/order.go
+++ order_srv/tests/order.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
 	"srvs/order_srv/proto"
 )
 
@@ -20,11 +22,11 @@ func Init() {
 	orderClient = proto.NewOrderClient(conn)
 }
 
-func TestCreateCartItem() {
+func TestCreateCartItem(userId int32, goodsId int32, nums int32) {
 	rsp, err := orderClient.CreateCartItem(context.Background(), &proto.CartItemRequest{
-		UserId:  1,
-		GoodsId: 421,
-		Nums:    1,
+		UserId:  userId,
+		GoodsId: goodsId,
+		Nums:    nums,
 	})
 	if err != nil {
 		panic(err)
@@ -32,10 +34,82 @@ func TestCreateCartItem() {
 	fmt.Println("CreateCartItem success, ID:", rsp.Id)
 }
 
+func TestCartItemList(userId int32) {
+	rsp, err := orderClient.CartItemList(context.Background(), &proto.UserInfo{
+		Id: userId,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range rsp.Data {
+		fmt.Println(item.Id, item.GoodsId, item.Nums)
+	}
+}
+
+func TestUpdateCartItem(id int32, nums int32) {
+	_, err := orderClient.UpdateCartItem(context.Background(), &proto.CartItemRequest{
+		Id:      id,
+		Nums:    nums,
+		Checked: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestCreateOrder(userId int32) {
+	rsp, err := orderClient.CreateOrder(context.Background(), &proto.OrderRequest{
+		UserId:  userId,
+		Address: "广东省广州市",
+		Name:    fmt.Sprintf("Zeng#%d", userId),
+		Mobile:  "18888888888",
+		Post:    "请尽快发货",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("CreateOrder success, ID:", rsp.Id)
+}
+
+func TestOrderDetail(orderId int32) {
+	rsp, err := orderClient.OrderDetail(context.Background(), &proto.OrderRequest{
+		Id: orderId,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("OrderSn:", rsp.OrderInfo.OrderSn)
+	for _, item := range rsp.Goods {
+		fmt.Println(item.GoodsName, item.GoodsPrice, item.Nums)
+	}
+}
+
+func TestOrderList() {
+	rsp, err := orderClient.OrderList(context.Background(), &proto.OrderFilterRequest{
+		UserId: 1,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, order := range rsp.Data {
+		fmt.Println(order.OrderSn, order.Total)
+	}
+
+}
+
 func main() {
 	Init()
 
-	TestCreateCartItem()
+	//TestCreateCartItem(1, 422, 2)
+	//TestCartItemList(1)
+	//TestUpdateCartItem(3, 0)
+	//TestCreateOrder(1)
+	//TestOrderDetail(5)
+	TestOrderList()
 
 	conn.Close()
 }
